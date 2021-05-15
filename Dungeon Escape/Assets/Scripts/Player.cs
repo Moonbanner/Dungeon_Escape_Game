9f9066095bf1135b6fcb76ac5a96834f0576b06b
Moonbanner <tltytb@gmail.com>
2021-05-15 13:06:44 +0700

    Initial Project upload

---
+++ Dungeon Escape/Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D _rigid;
+    [SerializeField] 
+    private float _jumpForce = 8.0f;
+    private bool _resetJump = false;
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    private PlayerAnimation _playerAnim;
+    private SpriteRenderer _playerSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        //assign handle to playerAnimation
+        _playerAnim = GetComponent<PlayerAnimation>();
+        _playerSprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if (move > 0)
+        {
+            Flip(false);
+        }
+        else if (move < 0)
+        {
+            Flip(true);
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && IsGrounded() == true)
+        {
+            Debug.Log("Jump!");
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            StartCoroutine(ResetJumpRoutine());
+        }
+
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+        _playerAnim.Move(move);
+    }
+
+    void Flip(bool faceLeft)
+    {
+        if (faceLeft == false)
+            _playerSprite.flipX = false;
+        else
+            _playerSprite.flipX = true;
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.7f, 1 << 8);
+
+        if (hitInfo.collider != null)
+        {
+            if (_resetJump == false)
+                return true;
+        }
+        return false;
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+    }
+}
