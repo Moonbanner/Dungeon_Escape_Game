2c07d2f72c4baea93f2d4ac920dab7f0b42efb77
DRyu1412 <72917241+DRyu1412@users.noreply.github.com>
2022-05-10 18:36:42 +0700

    Add Damage system for player

--- Dungeon Escape/Assets/Scripts/Enemy/Moss_Giant.cs
+++ Dungeon Escape/Assets/Scripts/Enemy/Moss_Giant.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moss_Giant : Enemy
+public class Moss_Giant : Enemy, IDamageable
 {
+    public int Health { get; set; }
+  
     // use for initialization
     public override void Init()
     {
         base.Init();
+    }
+
+    public void Damage()
+    {
+
     }
 }
