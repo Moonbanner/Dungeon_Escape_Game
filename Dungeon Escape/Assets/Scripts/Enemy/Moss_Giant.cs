d553e0de57fde5871a95fdecd6a930f261270001
DRyu1412 <72917241+DRyu1412@users.noreply.github.com>
2022-05-10 13:53:06 +0700

    Add Spider enemy

--- Dungeon Escape/Assets/Scripts/Enemy/Moss_Giant.cs
+++ Dungeon Escape/Assets/Scripts/Enemy/Moss_Giant.cs
@@ -6,10 +6,12 @@ public class Moss_Giant : Enemy
 {
     private Vector3 _currentTarget;
     private Animator _anim;
+    private SpriteRenderer _mossSprite; 
 
     private void Start()
     {
         _anim = GetComponentInChildren<Animator>();
+        _mossSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void Update()
@@ -23,14 +25,24 @@ public class Moss_Giant : Enemy
 
     void Movement()
     {
+        if(_currentTarget == pointA.position)
+        {
+            _mossSprite.flipX = true;
+        }
+        else if(_currentTarget == pointB.position)
+        {
+            _mossSprite.flipX = false;
+        }
+
         if (transform.position == pointA.position)
         {
             _currentTarget = pointB.position;
-
+            _anim.SetTrigger("Idle");
         }
         else if (transform.position == pointB.position)
         {
             _currentTarget = pointA.position;
+            _anim.SetTrigger("Idle");
         }
 
 
