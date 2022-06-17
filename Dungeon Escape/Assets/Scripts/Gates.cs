452f2643a68ca2afa80b5627c64734de3bf74762
DRyu1412 <72917241+DRyu1412@users.noreply.github.com>
2022-06-17 17:04:15 +0700

    game complete

---
+++ Dungeon Escape/Assets/Scripts/Gates.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Gates : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(SetGateState());
+        }
+    }
+
+    IEnumerator SetGateState()
+    {      
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Menu");
+    }
+}
