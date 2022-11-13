bb9704c2592aebee893f97936e25d588af0f441b
DRyu1412 <72917241+DRyu1412@users.noreply.github.com>
2022-11-14 05:57:05 +0700

    shop completed

--- Dungeon Escape/Assets/Scripts/Gates.cs
+++ Dungeon Escape/Assets/Scripts/Gates.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Gates : MonoBehaviour
 {
+    public GameObject HasKeyNotification;
+    public GameObject NoKeyNotification;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            StartCoroutine(SetGateState());
+            if (GameManager.Instance.HasKeyToCave)
+            {
+                HasKeyNotification.SetActive(true);
+            }
+            else
+            {
+                NoKeyNotification.SetActive(true);
+            }          
         }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            HasKeyNotification.SetActive(false);
+            NoKeyNotification.SetActive(false);
+        }
+    }
+
+    public void YesButtonClick()
+    {
+        StartCoroutine(SetGateState());
+        HasKeyNotification.SetActive(false);
+    }
+    public void NoButtonClick()
+    { 
+        HasKeyNotification.SetActive(false);
     }
 
     IEnumerator SetGateState()
