4cf4711f2989357f57cfbec8e4906aea1e0183a6
DRyu1412 <72917241+DRyu1412@users.noreply.github.com>
2022-12-05 22:09:10 +0700

    tutorial scene finished

---
+++ Dungeon Escape/Assets/Scripts/Gates/Gates.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Gates : MonoBehaviour
+{
+    public GameObject HasKeyNotification;
+    public GameObject NoKeyNotification;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (GameManager.Instance.HasKeyToCave)
+            {
+                HasKeyNotification.SetActive(true);
+            }
+            else
+            {
+                NoKeyNotification.SetActive(true);
+            }          
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            HasKeyNotification.SetActive(false);
+            NoKeyNotification.SetActive(false);
+        }
+    }
+
+    public void YesButtonClick()
+    {
+        StartCoroutine(SetGateState());
+        HasKeyNotification.SetActive(false);
+    }
+    public void NoButtonClick()
+    { 
+        HasKeyNotification.SetActive(false);
+    }
+
+    IEnumerator SetGateState()
+    {      
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("GameScene2");
+    }
+}
---
+++ Dungeon Escape/Assets/Scripts/Gates/TutorialGate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialGate : MonoBehaviour
+{
+    public GameObject EndTutorialNotification;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            EndTutorialNotification.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            EndTutorialNotification.SetActive(false);
+        }
+    }
+    public void YesButtonClick()
+    {
+        StartCoroutine(SetGateState());
+        EndTutorialNotification.SetActive(false);
+    }
+    public void NoButtonClick()
+    {
+        EndTutorialNotification.SetActive(false);
+    }
+
+    IEnumerator SetGateState()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Menu");
+    }
+}
