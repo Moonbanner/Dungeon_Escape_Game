f8c2a86c0d506f4a5cbcc3f8095b3c7a484b53d7
DRyu1412 <72917241+DRyu1412@users.noreply.github.com>
2022-12-06 14:12:39 +0700

    add tutorial note in menu

--- Dungeon Escape/Assets/Scripts/UI/MainMenu.cs
+++ Dungeon Escape/Assets/Scripts/UI/MainMenu.cs
@@ -8,13 +8,26 @@ public class MainMenu : MonoBehaviour
     private AudioSource _audioSource;
     [SerializeField]
     private AudioClip _menuSelect;
+    public GameObject PlayTutorialNotification;
     public void StartButton()
     {
         _audioSource = GetComponent<AudioSource>();
         _audioSource.PlayOneShot(_menuSelect);
-         SceneManager.LoadScene("GameScene");
+        PlayTutorialNotification.SetActive(true);
+         
     }
-
+    public void YesButtonClick()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.PlayOneShot(_menuSelect);
+        SceneManager.LoadScene("TutorialScene");
+    }
+    public void NoButtonClick()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.PlayOneShot(_menuSelect);
+        SceneManager.LoadScene("GameScene");
+    }
     public void QuitButton()
     {
         _audioSource = GetComponent<AudioSource>();
